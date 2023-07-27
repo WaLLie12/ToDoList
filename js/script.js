6da0838b48f28e38c06c7d7d21bf3712c3947064
vragno01 <vadimserapionov@gmail.com>
2023-07-27 20:42:10 +0300

    feat: add adding func

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ const input = document.querySelector('input')
 const wrapperForAddingElements = document.querySelector('ul') 
 
 addTask.addEventListener('click', () =>{
+    const newTask = input.value
+if(newTask){
     let list = document.createElement('li')
+    list.innerHTML = newTask
     wrapperForAddingElements.append(list)
-    
+    input.value = '';
+}   
 })
 
