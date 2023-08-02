daaa6d9911c5c9ff99145c5a745ab3fe2b809856
vragno01 <vadimserapionov@gmail.com>
2023-08-02 17:07:41 +0300

    feat: add remove button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+
 const addTask = document.querySelector('.input__button')
 const input = document.querySelector('input')
 const wrapperForAddingElements = document.querySelector('ul') 
 
-addTask.addEventListener('click', () =>{
+
+
+addTask.addEventListener('click', (event) =>{
     const newTask = input.value
 if(newTask){
     let list = document.createElement('li')
-    list.innerHTML = newTask
+    list.textContent = newTask
+    removeItem(list)
     wrapperForAddingElements.append(list)
     input.value = '';
-}   
+    }   
+})
 })
 
+function removeItem (element){
+    let removeBtn = document.createElement('button')
+    removeBtn.classList = 'remove'
+    removeBtn.textContent = 'remove'
+
+    element.append(removeBtn)
+}
