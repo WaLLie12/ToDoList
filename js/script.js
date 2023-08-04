77525567056d0d503e74ea26bdc3c8cacc920989
vragno01 <vadimserapionov@gmail.com>
2023-08-04 22:56:56 +0300

    feat: add remove and delete buttons

--- js/script.js
+++ js/script.js
@@ -1,27 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-const addTask = document.querySelector('.input__button')
-const input = document.querySelector('input')
-const wrapperForAddingElements = document.querySelector('ul') 
+    const addTask = document.querySelector('.input__button')
+    const input = document.querySelector('input')
+    const wrapperForAddingElements = document.querySelector('ul') 
+    const deleteBtn = document.querySelector('.btn__remove')
 
 
+    addTask.addEventListener('click', () =>{
+        const newTask = input.value
+        if(newTask){
+            const list = createEl(newTask)
+            wrapperForAddingElements.append(list);
+            input.value = '';
+            }   
+        })
 
-addTask.addEventListener('click', (event) =>{
-    const newTask = input.value
-if(newTask){
-    let list = document.createElement('li')
-    list.textContent = newTask
-    removeItem(list)
-    wrapperForAddingElements.append(list)
-    input.value = '';
-    }   
+    function createEl (element){
+        let list = document.createElement('li')
+        list.textContent = element
+        let removeBtn = document.createElement('button')
+        removeBtn.classList = 'remove'
+        removeBtn.textContent = 'remove'
+        removeBtn.addEventListener('click', () =>{
+            list.remove()
+        })
+        list.append(removeBtn)
+        return list
+    }
+
+    deleteBtn.addEventListener('click', () =>{
+        const listItems = document.querySelectorAll('li')
+        listItems.forEach(item =>{
+            item.remove()
+        })
+    })
 })
-})
-
-function removeItem (element){
-    let removeBtn = document.createElement('button')
-    removeBtn.classList = 'remove'
-    removeBtn.textContent = 'remove'
-
-    element.append(removeBtn)
-}
