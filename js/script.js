59e9a7a5a2c9069359da3209c9d4a2bead5b3ff5
vragno01 <vadimserapionov@gmail.com>
2023-10-19 16:42:40 +0300

    feat: add hide button logic

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const input = document.querySelector('input')
     const wrapperForAddingElements = document.querySelector('ul') 
     const deleteBtn = document.querySelector('.btn__remove')
+    const hideBtn = document.querySelector('.hide__button')
+
+    let hideListItems = false
 
 
     addTask.addEventListener('click', () =>{
@@ -17,6 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function createEl (element){
         let list = document.createElement('li')
+        list.classList.add('list')
         list.textContent = element
         let removeBtn = document.createElement('button')
         removeBtn.classList = 'remove'
@@ -28,10 +32,23 @@ document.addEventListener("DOMContentLoaded", () => {
         return list
     }
 
-    deleteBtn.addEventListener('click', () =>{
+    deleteBtn.addEventListener('submit', () =>{
         const listItems = document.querySelectorAll('li')
         listItems.forEach(item =>{
             item.remove()
         })
     })
+
+    function hideList(){
+        if(hideListItems){
+            wrapperForAddingElements.style.display = 'block'
+            hideBtn.innerHTML = 'Hide list'
+        } else{
+            wrapperForAddingElements.style.display = 'none'
+            hideBtn.innerHTML = 'Show list'
+        }
+        hideListItems = !hideListItems;
+    }
+
+    hideBtn.addEventListener('click', (hideList))
 })
